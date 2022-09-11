b7bba10d16a29fa560306b636602d7337f467708
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-11 14:53:19 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,18 @@
  */
 int main(void)
 {
-	int c;
+int c;
 
-	for (c = '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+	if (c < '9')
 	{
-			putchar(c);
-				if (c < '9')
-					{
-						putchar(',');
-								putchar(' ');
-										}
-					else
-								putchar('\n');
+		putchar(',');
+		putchar(' ');
 	}
-	return (0);
+	else
+		putchar('\n');
+}
+return (0);
 }
