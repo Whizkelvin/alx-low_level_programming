7ddd5aea689501dfeae4165727215a1ac228cd5a
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-11 11:17:01 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,14 +4,14 @@
 
 /**
  *   main - Entry point
- *   
+ *
  *     Return: Always 0 (Success)
  */
 int main(void)
 {
 	int c;
 
-	for(c = '0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
