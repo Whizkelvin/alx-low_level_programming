43a1183300be412bdd8ed82699288acaab2ebfee
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-11 14:00:06 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,4 @@
-#include <stdlib.hi>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -12,11 +12,9 @@ int main(void)
 	int c;
 
 	for (c = 0; c <= 9; c++)
-		
 	{
 			printf("%d", c);
 	}
-	
 	putchar('\n');
 	return (0);
 }
