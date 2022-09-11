af2cf79bcb2e6fc06beefd828f33d9744d58abfb
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-11 14:40:08 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -13,10 +13,10 @@ int main(void)
 {
 	int c;
 
-    for (c = 0; c <= 9; c++)
-{
-	printf("%d", c);
-}
-	putchar('\n')
-return (0);
+        for (c = 0; c <= 9; c++)
+        {
+		printf("%d", c);
+        }
+		putchar('\n')
+		return (0);
 }
