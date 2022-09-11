b55159f514a41b0d0878e1ddefdc57ed9a4fb058
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-11 15:01:36 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *   main - Entry point
+ *   
+ *     Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i < 100 ; i++)
+for (i = 0; i < 100 ; i++)
+{
+	if ((i / 10) < (i % 10))
 	{
-			if ((i / 10) < (i % 10))
-					{
-								putchar((i / 10) + '0');
-										putchar((i % 10) + '0');
-											}
-				if (i < 89 && (i / 10) < (i % 10))
-						{
-									putchar(',');
-											putchar(' ');
-												}
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 	}
-	putchar('\n');
-	return (0);
+	if (i < 89 && (i / 10) < (i % 10))
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
 }
