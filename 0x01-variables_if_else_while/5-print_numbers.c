4b5851300a3f911e296db512059545d433fb71cf
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-11 10:41:10 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  *	main - Prints the numbers since 0 to 9
- *   
+ *
  *	Return: Always (Success)
  */
 int main(void)
 {
 		char n;
 
-			for (n = '0'; n <= '9'; n++)
+					for (n = '0'; n <= '9'; n++)
 					{
 								putchar(n);
 									}
