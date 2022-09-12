0797af20ddba6151695314ba2917c96a24206901
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-12 05:19:31 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,30 +4,32 @@
 
 /**
  *   main - Entry point
- *   
+ *
  *     Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i = 0;
-	int a;
+int i = 0;
+int a;
 
-	for (a = 1; a < 100 ; a++)
+for (a = 1; a < 100 ; a++)
+{
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(' ');
+	putchar((a / 10) + '0');
+	putchar((a % 10) + '0');
+	if (i < 98)
 	{
-		putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-				putchar(' ');
-					putchar((a / 10) + '0');
-						putchar((a % 10) + '0');
-							if (i < 98)
-				{
-					putchar(',');
-						putchar(' ');
-								}
-							if (a == 99)
-							{													i++;
-					a = i;															}
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n');
-	return (0);
+	if (a == 99)
+	{
+		i++;
+		a = i;
+	}
+}
+putchar('\n');
+return (0);
 }
