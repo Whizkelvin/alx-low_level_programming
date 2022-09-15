314e95e7403aeed228adc2283eec4ba89a63984b
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-15 12:09:00 -0700

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
-int _isupper.c(int c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
