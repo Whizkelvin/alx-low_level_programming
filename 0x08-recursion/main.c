d8c4b7612b4df4a03a164ee44c0f11a72078aba4
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-09-29 11:30:42 -0700

    Inception. Is it possible?

---
+++ 0x08-recursion/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+   /*_puts_recursion ("Puts with recursion");*/
+     _print_rev_recursion("\nColton Walker");
+	return (0);
+
+}
