6d45c396dacc82a10e90f1e2ad4a0368d9f72be6
Whizkelvin <kelvinmelchizedek@gmail.com>
2022-10-06 08:11:23 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+File 0 - Write a function that allocates memory using malloc.
+File 1 - Write a function that concatenates two strings.
+File 2 - Write a function that allocates memory for an array, using malloc.
+File 3 - Write a function that creates an array of integers.
+File 4 - Write a function that reallocates a memory block using malloc and free.
+File 5 - Write a program that multiplies two positive numbers.
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*char prototype (char)
+*int protoypes (int)
+* putchar prototype (_putchar.c)
+*
+*
+*/
+int _putchar(char c)
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif /* MAIN_H */
